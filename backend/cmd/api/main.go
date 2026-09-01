0b7c0f1407272fa195bb0fdb2499826307be37f4
kiiikii <30403399+kiiikii@users.noreply.github.com>
2026-09-02 03:15:05 +0700

    chore(backend): moving main.go to cmd/api

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+type HealthResponse struct {
+	Status string `json:"status"`
+	Envi   string `json:"environment"`
+}
+
+type CreateOrderReq struct {
+	CustName string `json:"customer_name"`
+	ProdType string `json:"product_type"`
+}
+
+type Order struct {
+	ID       int    `json:"id"`
+	CustName string `json:"customer_name"`
+	ProdType string `json:"product_type"`
+	Status   string `json:"status"`
+}
+
+type App struct {
+	DB *sql.DB
+}
+
+func (app *App) HealthChecker(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	response := HealthResponse{
+		Status: "success",
+		Envi:   os.Getenv("APP_ENV"),
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func (app *App) CreateOrder(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req CreateOrderReq
+	var newID int
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+
+	if err != nil {
+		http.Error(w, "Invalid request Body", http.StatusBadRequest)
+		return
+	}
+
+	query := `INSERT INTO orders (customer_name, product_type) VALUES ($1, $2) RETURNING id`
+	err = app.DB.QueryRow(query, req.CustName, req.ProdType).Scan(&newID)
+	if err != nil {
+		http.Error(w, "Cannot insert data", http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]any{
+		"message":  "Order Created Successfully for " + req.CustName,
+		"order_id": newID,
+		"customer": req.CustName,
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func (app *App) OrdersRouter(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		app.GetOrders(w, r)
+	case http.MethodPost:
+		app.CreateOrder(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (app *App) GetOrders(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	query := `SELECT id, customer_name, product_type, status FROM orders ORDER BY id DESC`
+	rows, err := app.DB.Query(query)
+	if err != nil {
+		http.Error(w, "Database query Failed", http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+
+	var orders []Order = []Order{}
+
+	for rows.Next() {
+		var o Order
+
+		err := rows.Scan(&o.ID, &o.CustName, &o.ProdType, &o.Status)
+		if err != nil {
+			http.Error(w, "Error scanning order row", http.StatusInternalServerError)
+			return
+		}
+
+		orders = append(orders, o)
+	}
+
+	if err = rows.Err(); err != nil {
+		http.Error(w, "Error reading order rows", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(orders)
+}
+
+func main() {
+
+	//! Load godotenv
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+
+	//! Read the variable
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	//! Read APP_ENV
+	env := os.Getenv("APP_ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Failed to open DB: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Database is unreachable: %v", err)
+	}
+	app := &App{DB: db}
+
+	fmt.Println("Success connected to the database")
+
+	http.HandleFunc("/api/ping", app.HealthChecker)
+	http.HandleFunc("/api/v1/orders", app.OrdersRouter)
+
+	errs := http.ListenAndServe(":"+port, nil)
+	if errs != nil {
+		log.Fatalf("Server failed to start: %v", errs)
+	}
+
+	// fmt.Println("Server is Runinng on port: ", port)
+	// fmt.Printf("Current environtment: %s\n", env)
+}
--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,5 @@ module github.com/kiiikii/garment-erp/backend
 go 1.26.4
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/lib/pq v1.12.3 // indirect
